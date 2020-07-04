2498d8ec9b6c2de2eef3dd7b3075bd5fb27b735c
Bela Bothin <s6beboth@uni-bonn.de>
2020-07-04 16:38:14 +0200

    oops, (height change)

--- src/main/java/application/settingsChanger.java
+++ src/main/java/application/settingsChanger.java
@@ -6,7 +6,7 @@ package application;
 public class settingsChanger extends ControllerLogin {
         public settingsChanger() {
             try {
-                changeScene("MainScene.fxml", 1080,620, true, false, Variables.getX(), Variables.getY());
+                changeScene("MainScene.fxml", 1080,720, true, false, Variables.getX(), Variables.getY());
             } catch (Exception e) {
                 e.printStackTrace();
             }
